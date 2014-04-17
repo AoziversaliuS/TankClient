20ea003239e3f6cb456da67989154c69c5d2acca
AoziversaliuS <1220310838@qq.com>
2014-04-17 20:43:40 +0800

    客户端关闭虽然会报错但不影响服务端

--- src/oz/client/Client.java
+++ src/oz/client/Client.java
@@ -8,6 +8,8 @@ import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -21,7 +23,7 @@ import javax.swing.JFrame;
 import oz.bean.Tank;
 import oz.type.DirKey;
 
-public class Client extends JFrame implements Runnable,KeyListener{
+public class Client extends JFrame implements Runnable,KeyListener,WindowListener{
 	private static Toolkit tool = Toolkit.getDefaultToolkit();
 	
 	public static final int SCREEN_WIDTH = 600, SCREEN_HEIGHT = 600;
@@ -33,6 +35,8 @@ public class Client extends JFrame implements Runnable,KeyListener{
 	private ObjectInputStream ois;
 	private ObjectOutputStream oos;
 	private int id;
+	private boolean closed = false;
+	private int message = Tank.M_DEGFAULT;
 	
 	//MyTank
 	Image MyTank_Left;
@@ -67,9 +71,12 @@ public class Client extends JFrame implements Runnable,KeyListener{
 		this.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 		Dimension scrSize=Toolkit.getDefaultToolkit().getScreenSize(); 
 		this.setLocation(scrSize.width/2-SCREEN_WIDTH/2, scrSize.height/2-SCREEN_HEIGHT/2);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.addKeyListener(this);
 		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		
+		this.addKeyListener(this);
+		this.addWindowListener(this);
+		
 		this.setVisible(true);
 		
 		try {
@@ -122,24 +129,31 @@ public class Client extends JFrame implements Runnable,KeyListener{
 		
 		tank.active(selectKey, speed);
 		
+		//保存要发给服务器的信息
+		tank.setClientMessage(message);
 		
 		
 		
-		
-		
-		try {
-			//发送自己的坦克信息给服务器
-			oos.reset();
-			oos.writeObject(tank);
-			oos.flush();
-			//获取服务器数据
-			tanks = (ArrayList<Tank>) ois.readObject();
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+		if(!closed){
+//			System.out.println("还在");
+			try {
+				
+				//发送自己的坦克信息给服务器
+					oos.reset();
+					oos.writeObject(tank);
+					oos.flush();
+					//获取服务器数据
+					tanks = (ArrayList<Tank>) ois.readObject();
+					
+					
+			} catch (IOException e) {
+				System.exit(0);
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
 		}
+		
 	}
 	public void draw(){
 		
@@ -158,6 +172,19 @@ public class Client extends JFrame implements Runnable,KeyListener{
 					}
 					else if( tank.getLastDir()==DirKey.Down ){
 						gBuffer.drawImage(MyTank_Down, tank.getX(), tank.getY(), null);
+					}
+					//若服务器允许此客户端退出，则关闭与服务器的链接并退出
+					if( tank.getClientMessage()==Tank.M_EXIT_PERMIT ){
+						try {
+							oos.close();
+							ois.close();
+							socket.close();
+							closed = true;
+							System.exit(0);
+							System.out.println("退出了！！！！");
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
 					}
 				}
 				else{
@@ -243,8 +270,14 @@ public class Client extends JFrame implements Runnable,KeyListener{
 		}
 		
 	}
-	
-	
+	//窗口关闭
+	@Override
+	public void windowClosing(WindowEvent e) {
+		System.out.println("按下了退出按钮");
+		//设置信息为 请求退出
+		message = Tank.M_EXIT_REQUEST;
+		
+	}
 
 
 
@@ -253,7 +286,7 @@ public class Client extends JFrame implements Runnable,KeyListener{
 
 	@Override
 	public void run() {
-		while(true){
+		while(!closed){
 			
 			logic();
 			repaint();
@@ -309,6 +342,32 @@ public class Client extends JFrame implements Runnable,KeyListener{
 	}
 
 
+
+
+
+
+
+
+
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+	}
+	@Override
+	public void windowOpened(WindowEvent e) {
+	}
+	@Override
+	public void windowIconified(WindowEvent e) {
+	}
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+	}
+	@Override
+	public void windowActivated(WindowEvent e) {
+	}
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+	}
 	
 
 
